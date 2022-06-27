00514cc836d704b5938d38d674c0f0aba299c9e9
katherine.brock@dbtlabs.com <katherine.brock@dbtlabs.com>
2022-06-27 18:31:12 +0000

    added example incremental model

---
+++ models/example/my_incremental_dbt_model.sql
@@ -0,0 +1,24 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='id'
+    )
+}}
+
+with data as 
+
+( 
+
+select * 
+from {{ ref('my_first_dbt_model') }}
+
+{% if is_incremental() %}
+
+  where id >= (select max(id) from {{ this }})
+
+
+{% endif %}
+
+)
+
+select * from data
