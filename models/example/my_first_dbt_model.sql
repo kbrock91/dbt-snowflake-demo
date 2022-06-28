af321915cc81d576a707a5069506c49d798e2bae
Katherine Brock <48523663+kbrock91@users.noreply.github.com>
2022-06-28 10:36:57 -0400

    remove github action trigger (#12)
    * remove github action trigger
    
    * demo prep
    
    * add tests
    
    * doubled value on second dbt model

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -2,27 +2,27 @@ with source_data as
 
 (
     select
-    1 as id,
-    1 / 2 as math
+        1 as id,
+        1 / 2 as value
 
     union 
 
     select
         2 as id,
-        2 / 2 as math
+        2 / 2 as value
 
 
     union 
 
     select
         3 as id,
-        3 / 2 as math
+        3 / 2 as value
 
     union 
 
     select
         4 as id,
-        4 / 2 as math
+        4 / 2 as value
 )
 
 select * from source_data 
