2b3d8a1c512c3202c06dbe990b893151670c8feb
katherine.brock@dbtlabs.com <katherine.brock@dbtlabs.com>
2022-07-27 13:43:22 +0000

    added monthly rev agg

---
+++ models/marts/aggregates/monthly_gross_revenue.sql
@@ -0,0 +1,23 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+with order_items as 
+(
+    select * from {{ ref('fct_order_items') }}
+),
+
+customers as 
+(
+    select * from {{ ref('dim_customers') }}
+)
+
+select 
+    date_trunc(MONTH, order_items.order_date) as order_month, 
+    sum(order_items.gross_item_sales_amount) as gross_revenue
+from order_items
+left join customers 
+on order_items.customer_key = customers.customer_key
+group by 1
