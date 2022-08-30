dbb77918da755d7565eac10931eb8e8756390c31
Katherine Brock <katherine.brock@dbtlabs.com>
2022-08-30 19:59:08 +0000

    pre demo commits

--- models/marts/aggregates/monthly_gross_revenue.sql
+++ models/marts/aggregates/monthly_gross_revenue.sql
@@ -15,9 +15,10 @@ customers as
 )
 
 select 
+    --TODO: add customers.region,
     date_trunc(MONTH, order_items.order_date) as order_month, 
     sum(order_items.gross_item_sales_amount) as gross_revenue
 from order_items
 left join customers 
     on order_items.customer_key = customers.customer_key
-group by 1
+group by 1  --TODO: add , 2
