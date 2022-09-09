702c2c058aaa4756cad38a14a2aa5a30bb9ac118
Katherine Brock <katherine.brock@dbtlabs.com>
2022-09-09 13:28:18 +0000

    remove region

--- models/marts/aggregates/monthly_gross_revenue.sql
+++ models/marts/aggregates/monthly_gross_revenue.sql
@@ -15,10 +15,9 @@ customers as
 )
 
 select 
-    customers.region, 
     date_trunc(MONTH, order_items.order_date) as order_month, 
     sum(order_items.gross_item_sales_amount) as gross_revenue
 from order_items
 left join customers 
     on order_items.customer_key = customers.customer_key
-group by 1, 2
+group by 1
