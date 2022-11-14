1e3f68b67803c65b5dd69ed660274588f61c496a
kbrock91 <katherine.brock@dbtlabs.com>
2022-11-14 11:48:43 -0500

    artifacts

--- scripts/get_run_artifacts.py
+++ scripts/get_run_artifacts.py
@@ -1,9 +1,23 @@
 from dbtc import dbtCloudClient
 
-client = dbtCloudClient()
 
-artifacts = client.cloud.get_run_artifact(64729, 88853895,'manifest.json')
+def get_raw_sql(account_id, run_id, model_path):
 
-raw_sql = artifacts['nodes']['model.my_snowflake_dbt_project.part_suppliers']['raw_sql']
+    client = dbtCloudClient()
+
+    artifacts = client.cloud.get_run_artifact(account_id, run_id, 'manifest.json')
+
+    raw_sql = artifacts['nodes'][model_path]['raw_sql']
+
+    return raw_sql
+
+
+account_id = 64729
+
+run_id = 88853895
+
+model_path = 'model.my_snowflake_dbt_project.part_suppliers'
+
+raw_sql = get_raw_sql(account_id, run_id, model_path)
 
 print(raw_sql)
