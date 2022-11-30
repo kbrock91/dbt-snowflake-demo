cba317f6082bcd40febc314c4868629706c7b1c0
Katherine Brock <katherine.brock@dbtlabs.com>
2022-11-30 16:56:18 +0000

    test severity

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,17 +1,24 @@
-with source_data as 
+with
+    source_data as
 
-(
-    select
-        1 as id,
-        1 / 2 as value
+    (
+        select null as id, 1 / 2 as value
 
-    union 
+        union
 
-    select
-        2 as id,
-        2 / 2 as value
+        select 2 as id, 2 / 2 as value
 
-)
+        union
+
+        select 3 as id, 2 / 3 as value
+
+        union
+
+        select 4 as id, 2 / 4 as value
 
 
-select * from source_data
+    )
+
+
+select *
+from source_data
