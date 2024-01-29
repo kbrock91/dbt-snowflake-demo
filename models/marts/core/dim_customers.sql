fa8de6c51a49303a93d54a257a7571698a3f9308
katherine.brock@dbtlabs.com <katherine.brock@dbtlabs.com>
2024-01-29 14:55:37 +0000

    customer key = 1

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -36,7 +36,7 @@ region as (
 ),
 final as (
     select
-        customer.customer_key,
+        1  as customer_key, -- customer.customer_key,
         customer.name,
         customer.address,
         {# nation.nation_key as nation_key, #}
