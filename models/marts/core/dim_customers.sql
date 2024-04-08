591e455395a341614687eeecc4830e0adf81af81
katherine.brock@dbtlabs.com <katherine.brock@dbtlabs.com>
2024-04-08 16:46:43 +0000

    table

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -10,7 +10,7 @@ Full documentation: https://docs.getdbt.com/reference/resource-configs/snowflake
 
 {{
     config(
-        materialized='view',
+        materialized='table',
         transient=false
 
     )
