2854253b17a0cdfd5e4d7ad1207996cd3ba02013
Katherine Brock <48523663+kbrock91@users.noreply.github.com>
2025-03-12 09:21:02 -0500

    only change second_dbt_model (#147)

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -6,7 +6,7 @@
 
 select
     id,
-    value * 2 as doubled_value, --change to double_value
+    value * 4 as doubled_value, --change to double_value
     current_timestamp as _loaded_at
 from
     {{ ref('my_first_dbt_model') }}
