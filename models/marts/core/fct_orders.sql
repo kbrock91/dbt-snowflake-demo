f8f0dcfa9a0a423af56dcab8cbd51db5976d0b03
Katherine Brock <katherine.brock@dbtlabs.com>
2025-06-23 18:48:08 +0000

    changes

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -23,6 +23,7 @@ order_item_summary as (
         sum(item_discount_amount) as item_discount_amount,
         sum(item_tax_amount) as item_tax_amount,
         sum(net_item_sales_amount) as net_item_sales_amount
+      --  , sum(commit_date) as sum_commit
     from order_item
     group by
         1
